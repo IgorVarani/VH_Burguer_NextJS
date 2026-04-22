28161004d9fecb33fd796db51aae0b2b2fcc1f74
IgorVarani <igor.duo@aluno.senai.br>
2026-04-22 18:31:13 -0300

    :sparkles: feat: finalizando home

---
+++ vh-burguer/src/components/lista-produto/lista-produto.tsx
@@ -0,0 +1,29 @@
+import CardProduto from "../card-produto/card-produto";
+import styles from "./lista-produto.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSliders } from '@fortawesome/free-solid-svg-icons'
+
+const ListaProduto = () => {
+    return (
+        <>
+            <div id={styles.botoes_home}>
+                <button className={styles.botao}>Filtrar<FontAwesomeIcon icon={faSliders} /></button>
+                <div id={styles.botoes_direita}>
+                    <button className={styles.botao}>Promoções</button>
+                    <button className={styles.botao}>Produtos</button>
+                </div>
+            </div>
+            
+            <div id={styles.cards_produtos}>
+                <CardProduto />
+                <CardProduto />
+                <CardProduto />
+                <CardProduto />
+                <CardProduto />
+                <CardProduto />
+            </div>
+        </>
+    )
+}
+
+export default ListaProduto;
--- vh-burguer/src/pages/home/index.tsx
+++ vh-burguer/src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import styles from "./home.module.css"
 import { Fragment } from "react/jsx-runtime"
+import ListaProduto from "@/components/lista-produto/lista-produto";
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
                 <section id={styles.banner}>
                     <h1>BEM-VINDO(A) AO VH BURGUER</h1>
                     <img src="../imgs/Foto_de_Hamburgueres.svg" alt="Um grupo com três hambúrgueres abaixo do título" />
+                    
                     <div id={styles.botoes}>
                         <button id={styles.btn_chamar}>Chamar Atendente</button>
                         <button id={styles.btn_ver}>Ver Cardápio</button>
@@ -20,38 +22,44 @@ const Home = () => {
                 </section>
 
                 <section id={styles.destaques}>
-                    <div id={styles.container_esquerda}>
-                        <article id={styles.mais_pedidos}>
-                            <p className={styles.texto_article}>Os queridinhos da galera</p>
-                            <h2 className={styles.h2_article}>MAIS PEDIDOS</h2>
+                    <div className={`${styles.container_destaques} layout_guide`}>
+                        <article className={styles.card_destaque_mais_pedidos}>
+                            <p>Os queridinhos da galera</p>
+                            <p className={styles.destaque}>mais pedidos</p>
                         </article>
-                    </div>
-                    <div id={styles.container_direita}>
-                        <article id={styles.muito_bacon}>
-                            <p className={styles.texto_article}>Lanches com</p>
-                            <h2 className={styles.h2_article}>MUITO BACON</h2>
-                        </article>
-                        <article id={styles.super_combos}>
-                            <p className={styles.texto_article}>Se tiver com muita fome</p>
-                            <h2 className={styles.h2_article}>SUPER COMBOS</h2>
-                        </article>
+
+                        <div className={styles.cards_direita}>
+                            <article className={styles.card_destaque_muito_bacon}>
+                                <p>Lanches com</p>
+                                <p className={styles.destaque}>muito bacon</p>
+                            </article>
+                            
+                            <article className={styles.card_destaque_super_combos}>
+                                <p>Se tiver muita fome</p>
+                                <p className={styles.destaque}>Super combos</p>
+                            </article>
+                        </div>
                     </div>
                 </section>
 
                 <section id={styles.cardapio}>
-                    <h3>Cardápio</h3>
+                    <div className={`${styles.container_cardapio} layout_guide`}>
+                        <h2>CARDÁPIO</h2>
+                        <ListaProduto/>
+                    </div>
                 </section>
 
                 <section id={styles.unidades}>
-                    <div id={styles.container_unidades}>
-                        <img src="../imgs/Unidade.jpeg" alt="Uma foto retratando uma de nossas unidades" />
-                        <h4>NOSSAS UNIDADES</h4>
-                        <ul id={styles.lista_unidades}>
-                            <li>Centro - Av. Aurora, 742</li>
-                            <li>Jardim - Av. das Palmeiras, 1280</li>
-                            <li>Norte - Av. Horizonte, 305</li>
-                            <li>Sul - Av. Nova Esperança, 910</li>
-                        </ul>
+                    <div className={`${styles.container_unidades} layout_guide`}>
+                        <div className={styles.texto_unidades}>
+                            <h3>NOSSAS UNIDADES</h3>
+                            <ul className={styles.lista_unidades}>
+                                <li>Centro - Av. Aurora, 742</li>
+                                <li>Jardim - Av. das Palmeiras, 1280</li>
+                                <li>Norte - Av. Horizonte, 305</li>
+                                <li>Sul - Av. Nova Esperança, 910</li>
+                            </ul>
+                        </div>
                     </div>
                 </section>
             </main>
