08fcd29b5312e8229d29708f5cdfc6b51e458b4d
IgorVarani <igor.duo@aluno.senai.br>
2026-04-28 08:22:48 -0300

    :truck: chore: atualizando o endereco local

--- vh-burguer/src/pages/api/authService.ts
+++ vh-burguer/src/pages/api/authService.ts
@@ -1,13 +1,15 @@
 import { api } from "./api";
 export async function login(email: string, senha: string){
-    try{
-        //requisição:
+    try
+    {
+        //? Requisição:
         const response = await api.post("Autenticacao/login", {email, senha});
 
-        console.log("eba deu certo");
+        console.log("Autorização concluída com sucesso!");
         console.log(response.data.token);
-
-    }catch(error: any){
-        throw new Error("Email ou senha inválidos");
+    }
+    catch(error: any)
+    {
+        throw new Error("Email ou senha inválidos.");
     }
 }
