a6425d57617434ed748711b0105aa169b59bf022
IgorVarani <igor.duo@aluno.senai.br>
2026-04-29 10:35:07 -0300

    :construction: feat: integrando API no produto

--- vh-burguer/src/pages/api/categoriaService.ts
+++ vh-burguer/src/pages/api/categoriaService.ts
@@ -1,9 +1,23 @@
 import { api } from "./api";
 
-export async function cadastrarCategoria(nome: string) {
+export async function cadastrarCategoria(nome: string)
+{
     try
     {
         await api.post("Categoria", {nome})
+    }
+    catch(error: any)
+    {
+        throw new Error(error.response.data);
+    }
+}
+
+export async function listarCategoria()
+{
+    try
+    {
+        const response = await api.get("Categoria");
+        return response;
     }
     catch(error: any)
     {
--- vh-burguer/src/pages/produto/index.tsx
+++ vh-burguer/src/pages/produto/index.tsx
@@ -3,8 +3,36 @@ import HeaderMini from "@/components/header-mini/header-mini";
 import { Fragment } from "react/jsx-runtime"
 import styles from "./produto.module.css"
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { listarCategoria } from "../api/categoriaService";
+
+interface Categoria
+{
+    categoriaID: number,
+    nome: string
+}
 
 const Produto = () => {
+
+    const[categorias, setCategorias] = useState<Categoria[]>([]);
+
+    const[nome, setNome] = useState<string>("");
+    const[descricao, setDescricao] = useState<string>("");
+    const[preco, setPreco] = useState<string>("");
+    const[imagem, setImagem] = useState<File | null>(null);
+    const[categoriasSelecionadas, setCategoriasSelecionadas] = useState<number[]>([]);
+
+    async function listarCategoriaEmProduto()
+    {
+        const lista = await listarCategoria();
+        setCategorias(lista.data);
+    }
+
+    //? Quando o produto for renderizado, a função "listarCategoriaEmProduto" ocorrerá.
+    useEffect(() => {
+        listarCategoriaEmProduto();
+    }, [])
+
     return (
         <Fragment>
             <HeaderMini/>
@@ -13,33 +41,34 @@ const Produto = () => {
                 <form id={styles.form_container}>
                     <div className={styles.div_agrupar}>
                         <span>Nome do Produto</span>
-                        <input type="text" placeholder="Big Monster"/>
+                        <input type="text" placeholder="Big Monster"
+                        value={nome} onChange={(e) => setNome(e.target.value)}/>
                     </div>
 
                     <div className={styles.div_agrupar}>
                         <span>Descrição</span>
-                        <input id={styles.input_des} type="text" placeholder="Uma versão superior do clássico Monster, só que MUITO maior."/>
+                        <input id={styles.input_des} type="text" placeholder="Uma versão superior do clássico Monster, só que MUITO maior."
+                        value={descricao} onChange={(e) => setDescricao(e.target.value)}/>
                     </div>
 
                     <div className={styles.div_agrupar}>
                         <span>Preço (R$)</span>
-                        <input type="number" placeholder="50.00"/>
+                        <input type="text" placeholder="50.00"
+                        value={preco} onChange={(e) => setPreco(e.target.value)}/>
                     </div>
 
                     <div className={styles.div_agrupar}>
                         <span>Categoria</span>
-                        <select required>
-                            <option value="" disabled selected hidden>Selecione a Categoria</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                        <select multiple onChange={(e) =>
+                        setCategoriasSelecionadas(Array.from(e.target.selectedOptions).map((option) => Number(option.value)))}>
+                            {categorias.map((item) => (<option value={item.categoriaID} key={item.categoriaID}>{item.nome}</option>))}
                         </select>
                         <Link id={styles.link} href="/categoria">Adicionar Categoria</Link>
                     </div>
 
                     <div className={styles.div_agrupar}>
                         <span>URL da Imagem</span>
-                        <input type="text" placeholder="https://unsplash.com/pt-br/fotografias/big_monster"/>
+                        <input type="file" onChange={(e) => {if(e.target.files && e.target.files[0])(setImagem(e.target.files[0]))}}/>
                     </div>
 
                     <div id={styles.botoes}>
